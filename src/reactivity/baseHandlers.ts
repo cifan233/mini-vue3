af65c99c2ebc87be2f0011527ef0db2752d2abd0
zqq.dg <zqq.dg@qq.com>
2023-02-02 15:02:06 +0800

    实现 reactive 和 readonly 嵌套对象转换功能

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,6 @@
-import { track,trigger } from "./effect"
-import { ReactiveFlags } from "./reactive"
+import { track, trigger } from "./effect"
+import { ReactiveFlags, reactive, readonly } from "./reactive"
+import { isObject } from "../shared"
 
 //优化 只初始化时处理一遍
 const get = createGetter()
@@ -7,27 +8,33 @@ const set = createSetter()
 
 const readonlyGet = createGetter(true)
 
-function createGetter(isReadonly = false){
-  return function get(target,key){
-    if(key === ReactiveFlags.IS_REACTIVE){
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly
-    }else if(key === ReactiveFlags.IS_READONLY){
+    } else if (key === ReactiveFlags.IS_READONLY) {
       return isReadonly
     }
-    const res = Reflect.get(target,key)
-    if(!isReadonly){
+    const res = Reflect.get(target, key)
+    if (!isReadonly) {
       //收集依赖
-      track(target,key)
+      track(target, key)
     }
+
+    //如果是对象,处理嵌套对象
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res)
+    }
+
     return res
   }
 }
 
-function createSetter(){
-  return function set(target, key, value){
-    const res = Reflect.set(target,key,value)
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value)
     //触发依赖
-    trigger(target,key)
+    trigger(target, key)
     return res
   }
 }
@@ -39,9 +46,9 @@ export const mutableHandlers = {
 }
 
 export const readonlyHandlers = {
-  get:readonlyGet,
-  set(target, key, value){
-    console.warn(`${key} set 失败，因为target是readonly`,target)
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`${key} set 失败，因为target是readonly`, target)
     return true
   }
 }
--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,18 +1,20 @@
 
-import { readonly,isReadonly } from '../reactive'
+import { readonly, isReadonly } from '../reactive'
 
-describe('readonly',()=>{
-  it('happy path',()=>{
-    const original = {foo:1,bar:{baz:2}}
+describe('readonly', () => {
+  it('happy path', () => {
+    const original = { foo: 1, bar: { baz: 2 } }
     const wrapped = readonly(original)
     expect(wrapped).not.toBe(original)
     expect(wrapped.foo).toBe(1)
     expect(isReadonly(wrapped)).toBe(true)
     expect(isReadonly(original)).toBe(false)
+    expect(isReadonly(wrapped.bar)).toBe(true)
+    expect(isReadonly(original.bar)).toBe(false)
   })
 
-  it('should warn when call set',()=>{
-    const user = readonly({ age:10 })
+  it('should warn when call set', () => {
+    const user = readonly({ age: 10 })
     console.warn = jest.fn()
     user.age = 11
 
